f5eae8474f1dc6a7ba5c8fea339a3c75bc173095
Aparna-VK <aparnav@qburst.com>
2018-08-02 11:01:21 +0530

    added folder routerAssignment

---
+++ routerAssignment/src/app.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Form from "./Form";
+import ViewData from "./ViewData";
+
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Link to ="/">Home</Link>
+          <br/>
+          <Link to ="/form">Enter form details</Link>
+            <br/>       
+          <Link to ="/viewData">View Data</Link>
+          <br/>
+          <br/>
+          <hr/>
+          <Route path="/" component={home}/>
+          <Route path="/form" component={Form} /> 
+          <Route path="/viewData" component={ViewData} /> 
+        </div>
+      </Router>
+
+    );
+  }
+}
+
+const home = () => (
+<div>
+
+</div>
+);
+
+export default App
---
+++ routerAssignment/src/reducers/formSubmit.js
@@ -0,0 +1,22 @@
+const initialState = {
+  firstName :"",
+  lastName: "",
+  contactNumber:"",
+  address:""
+};
+const formSubmit = (state = initialState, action) => {
+  console.log(action.firstName);
+  switch (action.type) {
+    case 'BUTTON_CLICKED':
+      return {
+        ...state,
+        firstName: action.firstName1,
+        lastName: action.lastName1,
+        contactNumber: action.contactNumber1,
+        address: action.address1,
+            };
+    default:
+      return state;
+  }
+}
+export default formSubmit;
